546fbb578d84d69ae484716e4b7f780bdaf1686a
Andrew Efremov <56955307+ka1tzyu@users.noreply.github.com>
2023-02-04 14:35:12 +0200

    fix config service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,21 @@
 import {Module} from '@nestjs/common';
+import {ConfigModule, ConfigService} from '@nestjs/config';
 import configuration from "./config/configuration";
 import {MongooseModule} from "@nestjs/mongoose";
 import {ApartmentModule} from "./apartment/apartment.module";
 
 @Module({
   imports: [
-    MongooseModule.forRoot(configuration().database.connectionString),
-    ApartmentModule
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule.forRoot({load: [configuration]})],
+      useFactory: (configService: ConfigService) => ({uri: configService.get<string>('database.connectionString')}),
+      inject: [ConfigService],
+    }),
+    ApartmentModule,
   ],
   controllers: [],
   providers: [],
 })
+
 export class AppModule {
 }
--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -2,6 +2,6 @@ import * as process from 'process';
 
 export default () => ({
   database: {
-    connectionString: process.env.DATABASE_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/apartments',
+    connectionString: process.env.DATABASE_CONNECTION_STRING,
   }
 });
